8ae0be71ff3aedbed24efbe75e02936ed0f2cb15
akashtiwari-bizcloudexperts <akash.tiwari@bizcloudexperts.com>
2022-11-22 02:09:36 +0530

    .

--- dms-full-load-task.tf
+++ dms-full-load-task.tf
@@ -2,7 +2,6 @@ resource "aws_dms_replication_task" "omni-wt-rt-header-fullload-dms-task" {
   migration_type            = "full-load"
   replication_instance_arn  = aws_dms_replication_instance.omni-wt-rt-updates-dms-instance.replication_instance_arn
   replication_task_id       = "omni-wt-rt-header-fullload-${var.env}"
-  replication_task_settings = "..."
   source_endpoint_arn       = aws_dms_endpoint.omni-wt-rt-updates-source-endpoint.endpoint_arn
   table_mappings            = "{\"rules\":[{\"rule-type\":\"selection\",\"rule-id\":\"1\",\"rule-name\":\"1\",\"object-locator\":{\"schema-name\":\"dbo\",\"table-name\":\"tbl_ShipmentHeader\"},\"rule-action\":\"include\"}]}"
  target_endpoint_arn = aws_dms_endpoint.omni-wt-rt-updates-target-endpoint.endpoint_arn
