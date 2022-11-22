df337a46ee3c221141fcd39387f15abcb4d69b55
akashtiwari-bizcloudexperts <akash.tiwari@bizcloudexperts.com>
2022-11-22 21:33:52 +0530

    added task settings and cloudwatch loggroup

--- dms-cdc-task.tf
+++ dms-cdc-task.tf
@@ -2,6 +2,185 @@ resource "aws_dms_replication_task" "omni-wt-rt-cdc" {
   migration_type            = "cdc"
   replication_instance_arn  = aws_dms_replication_instance.omni-wt-rt-updates-dms-instance.replication_instance_arn
   replication_task_id       = "omni-wt-rt-${var.env}"
+  replication_task_settings = jsonencode(
+    {
+    "Logging": {
+        "EnableLogging": true,
+        "LogComponents": [
+            {
+                "Severity": "LOGGER_SEVERITY_DEFAULT",
+                "Id": "TRANSFORMATION"
+            },
+            {
+                "Severity": "LOGGER_SEVERITY_DEFAULT",
+                "Id": "SOURCE_UNLOAD"
+            },
+            {
+                "Severity": "LOGGER_SEVERITY_DEFAULT",
+                "Id": "IO"
+            },
+            {
+                "Severity": "LOGGER_SEVERITY_DEFAULT",
+                "Id": "TARGET_LOAD"
+            },
+            {
+                "Severity": "LOGGER_SEVERITY_DEFAULT",
+                "Id": "PERFORMANCE"
+            },
+            {
+                "Severity": "LOGGER_SEVERITY_DEFAULT",
+                "Id": "SOURCE_CAPTURE"
+            },
+            {
+                "Severity": "LOGGER_SEVERITY_DEFAULT",
+                "Id": "SORTER"
+            },
+            {
+                "Severity": "LOGGER_SEVERITY_DEFAULT",
+                "Id": "REST_SERVER"
+            },
+            {
+                "Severity": "LOGGER_SEVERITY_DEFAULT",
+                "Id": "VALIDATOR_EXT"
+            },
+            {
+                "Severity": "LOGGER_SEVERITY_DEFAULT",
+                "Id": "TARGET_APPLY"
+            },
+            {
+                "Severity": "LOGGER_SEVERITY_DEFAULT",
+                "Id": "TASK_MANAGER"
+            },
+            {
+                "Severity": "LOGGER_SEVERITY_DEFAULT",
+                "Id": "TABLES_MANAGER"
+            },
+            {
+                "Severity": "LOGGER_SEVERITY_DEFAULT",
+                "Id": "METADATA_MANAGER"
+            },
+            {
+                "Severity": "LOGGER_SEVERITY_DEFAULT",
+                "Id": "FILE_FACTORY"
+            },
+            {
+                "Severity": "LOGGER_SEVERITY_DEFAULT",
+                "Id": "COMMON"
+            },
+            {
+                "Severity": "LOGGER_SEVERITY_DEFAULT",
+                "Id": "ADDONS"
+            },
+            {
+                "Severity": "LOGGER_SEVERITY_DEFAULT",
+                "Id": "DATA_STRUCTURE"
+            },
+            {
+                "Severity": "LOGGER_SEVERITY_DEFAULT",
+                "Id": "COMMUNICATION"
+            },
+            {
+                "Severity": "LOGGER_SEVERITY_DEFAULT",
+                "Id": "FILE_TRANSFER"
+            }
+        ],
+        "CloudWatchLogGroup": "${aws_cloudwatch_log_group.omni-wt-rt-loggroup.name}",
+        "CloudWatchLogStream": "${aws_cloudwatch_log_stream.omni-wt-rt-loggroup-stream.name}"
+    },
+    "StreamBufferSettings": {
+        "StreamBufferCount": 3,
+        "CtrlStreamBufferSizeInMB": 5,
+        "StreamBufferSizeInMB": 8
+    },
+    "ErrorBehavior": {
+        "FailOnNoTablesCaptured": false,
+        "ApplyErrorUpdatePolicy": "LOG_ERROR",
+        "FailOnTransactionConsistencyBreached": false,
+        "RecoverableErrorThrottlingMax": 1800,
+        "DataErrorEscalationPolicy": "SUSPEND_TABLE",
+        "ApplyErrorEscalationCount": 0,
+        "RecoverableErrorStopRetryAfterThrottlingMax": false,
+        "RecoverableErrorThrottling": true,
+        "ApplyErrorFailOnTruncationDdl": false,
+        "DataTruncationErrorPolicy": "LOG_ERROR",
+        "ApplyErrorInsertPolicy": "LOG_ERROR",
+        "EventErrorPolicy": "IGNORE",
+        "ApplyErrorEscalationPolicy": "LOG_ERROR",
+        "RecoverableErrorCount": -1,
+        "DataErrorEscalationCount": 0,
+        "TableErrorEscalationPolicy": "STOP_TASK",
+        "RecoverableErrorInterval": 5,
+        "ApplyErrorDeletePolicy": "IGNORE_RECORD",
+        "TableErrorEscalationCount": 0,
+        "FullLoadIgnoreConflicts": true,
+        "DataErrorPolicy": "LOG_ERROR",
+        "TableErrorPolicy": "SUSPEND_TABLE"
+    },
+    "TTSettings": {
+        "TTS3Settings": null,
+        "TTRecordSettings": null,
+        "EnableTT": false
+    },
+    "FullLoadSettings": {
+        "CommitRate": 10000,
+        "StopTaskCachedChangesApplied": false,
+        "StopTaskCachedChangesNotApplied": false,
+        "MaxFullLoadSubTasks": 20,
+        "TransactionConsistencyTimeout": 600,
+        "CreatePkAfterFullLoad": false,
+        "TargetTablePrepMode": "DO_NOTHING"
+    },
+    "TargetMetadata": {
+        "ParallelApplyBufferSize": 0,
+        "ParallelApplyQueuesPerThread": 0,
+        "ParallelApplyThreads": 0,
+        "TargetSchema": "",
+        "InlineLobMaxSize": 0,
+        "ParallelLoadQueuesPerThread": 0,
+        "SupportLobs": true,
+        "LobChunkSize": 0,
+        "TaskRecoveryTableEnabled": false,
+        "ParallelLoadThreads": 0,
+        "LobMaxSize": 32,
+        "BatchApplyEnabled": false,
+        "FullLobMode": false,
+        "LimitedSizeLobMode": true,
+        "LoadMaxFileSize": 0,
+        "ParallelLoadBufferSize": 0
+    },
+    "BeforeImageSettings": null,
+    "ControlTablesSettings": {
+        "historyTimeslotInMinutes": 5,
+        "HistoryTimeslotInMinutes": 5,
+        "StatusTableEnabled": false,
+        "SuspendedTablesTableEnabled": false,
+        "HistoryTableEnabled": false,
+        "ControlSchema": "",
+        "FullLoadExceptionTableEnabled": false
+    },
+    "LoopbackPreventionSettings": null,
+    "CharacterSetSettings": null,
+    "FailTaskWhenCleanTaskResourceFailed": false,
+    "ChangeProcessingTuning": {
+        "StatementCacheSize": 50,
+        "CommitTimeout": 1,
+        "BatchApplyPreserveTransaction": true,
+        "BatchApplyTimeoutMin": 1,
+        "BatchSplitSize": 0,
+        "BatchApplyTimeoutMax": 30,
+        "MinTransactionSize": 1000,
+        "MemoryKeepTime": 60,
+        "BatchApplyMemoryLimit": 500,
+        "MemoryLimitTotal": 1024
+    },
+    "ChangeProcessingDdlHandlingPolicy": {
+        "HandleSourceTableDropped": true,
+        "HandleSourceTableTruncated": true,
+        "HandleSourceTableAltered": true
+    },
+    "PostProcessingRules": null
+}
+  )
   source_endpoint_arn       = var.source_endpoint_arn
   target_endpoint_arn       = aws_dms_endpoint.omni-wt-rt-updates-target-endpoint-cdc.endpoint_arn
   table_mappings            = jsonencode(
