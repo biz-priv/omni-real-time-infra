4fa454d8ef63e4b4cc58f6a416cb152699fcefba
akashtiwari-bizcloudexperts <akash.tiwari@bizcloudexperts.com>
2022-11-23 18:51:26 +0530

    solved dms task settings modification issue

--- dms-full-load-task.tf
+++ dms-full-load-task.tf
@@ -176,6 +176,11 @@ resource "aws_dms_replication_task" "omni-wt-rt-header-fullload-dms-task" {
     "TTSettings": null
 }
   )
+  lifecycle {
+	  ignore_changes = [
+      replication_task_settings,
+     ]
+  }
   # source_endpoint_arn      = aws_dms_endpoint.omni-wt-rt-updates-source-endpoint.endpoint_arn
   source_endpoint_arn = var.source_endpoint_arn
   table_mappings      = "{\"rules\":[{\"rule-type\":\"selection\",\"rule-id\":\"1\",\"rule-name\":\"1\",\"object-locator\":{\"schema-name\":\"dbo\",\"table-name\":\"tbl_ShipmentHeader\"},\"rule-action\":\"include\"}]}"
