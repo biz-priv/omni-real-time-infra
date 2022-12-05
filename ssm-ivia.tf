97bc942db56c2d61e7139bd61a49ea927e4e2879
akashtiwari-bizcloudexperts <akash.tiwari@bizcloudexperts.com>
2022-12-06 00:26:09 +0530

    added ssm for ivia

---
+++ ssm-ivia.tf
@@ -0,0 +1,77 @@
+resource "aws_ssm_parameter" "omni-ivia-ddb-name" {
+  name  = "/omni-ivia/${var.env}/ddb.tableName"
+  type  = "String"
+  value = var.omni_ivia_ddb_name
+
+  tags = {
+    Application = "Real Time Updates"
+    CreatedBy   = "BizCloudExperts"
+    Environment = var.env
+    STAGE       = var.env
+  }
+}
+
+resource "aws_ssm_parameter" "omni-ivia-createShipment-url" {
+  name  = "/omni-ivia/${var.env}/createShipment/url"
+  type  = "String"
+  value = var.omni_ivia_createShipment_url
+
+  tags = {
+    Application = "Real Time Updates"
+    CreatedBy   = "BizCloudExperts"
+    Environment = var.env
+    STAGE       = var.env
+  }
+}
+
+resource "aws_ssm_parameter" "omni-ivia-createShipment-token" {
+  name  = "/omni-ivia/${var.env}/createShipment/token"
+  type  = "String"
+  value = var.omni_ivia_createShipment_token
+
+  tags = {
+    Application = "Real Time Updates"
+    CreatedBy   = "BizCloudExperts"
+    Environment = var.env
+    STAGE       = var.env
+  }
+}
+
+resource "aws_ssm_parameter" "omni-ivia-createShipment-xmlUpdate-url" {
+  name  = "/omni-ivia/${var.env}/createShipment/xmlUpdate/url"
+  type  = "String"
+  value = var.omni_ivia_createShipment_xmlUpdate_url
+
+  tags = {
+    Application = "Real Time Updates"
+    CreatedBy   = "BizCloudExperts"
+    Environment = var.env
+    STAGE       = var.env
+  }
+}
+
+resource "aws_ssm_parameter" "omni-ivia-createShipment-xmlUpdate-userId" {
+  name  = "/omni-ivia/${var.env}/createShipment/xmlUpdate/userId"
+  type  = "String"
+  value = var.omni_ivia_createShipment_xmlUpdate_userId
+
+  tags = {
+    Application = "Real Time Updates"
+    CreatedBy   = "BizCloudExperts"
+    Environment = var.env
+    STAGE       = var.env
+  }
+}
+
+resource "aws_ssm_parameter" "omni-ivia-createShipment-xmlUpdate-password" {
+  name  = "/omni-ivia/${var.env}/createShipment/xmlUpdate/password"
+  type  = "String"
+  value = var.omni_ivia_createShipment_xmlUpdate_password
+
+  tags = {
+    Application = "Real Time Updates"
+    CreatedBy   = "BizCloudExperts"
+    Environment = var.env
+    STAGE       = var.env
+  }
+}
