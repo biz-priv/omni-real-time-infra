b5cbee630867562b826cf5034f623359e70d9694
vs002666 <ASHISH_AKSHANTAL@neimanmarcus.com>
2023-03-22 09:09:01 -0500

    corrected s3 notification for tbl_equipment

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -142,7 +142,7 @@ resource "aws_s3_bucket_notification" "omni-wt-rt-updates-s3-bucket-notification
   }
 
   queue {
-    queue_arn     = aws_sqs_queue.omni_wt_rt_queue[18].arn
+    queue_arn     = aws_sqs_queue.omni_wt_rt_queue[19].arn
     events        = ["s3:ObjectCreated:*"]
     filter_suffix = ".csv"
     filter_prefix = "dbo/${var.s3_notification_table_name[19]}"
