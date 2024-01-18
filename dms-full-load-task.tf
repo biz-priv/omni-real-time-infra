e841f67112a2bc404d08c613d0895ee7e85b3448
madhavamvs25 <madhava.matta@bizcloudexperts.com>
2024-01-18 23:23:17 +0530

    creating dms task for rateFile table

--- dms-full-load-task.tf
+++ dms-full-load-task.tf
@@ -499,3 +499,229 @@ resource "aws_dms_replication_task" "omni-wt-rt-header-fullload-dms-task" {
     STAGE       = var.env
   }
 }
+
+resource "aws_dms_replication_task" "omni-wt-rt-header-fullload-rateFile-dms-task" {
+  migration_type           = "full-load"
+  replication_instance_arn = aws_dms_replication_instance.omni-wt-rt-updates-dms-instance.replication_instance_arn
+  replication_task_id      = "omni-wt-rt-header-fullload-rateFile-${var.env}"
+  replication_task_settings = jsonencode(
+    {
+      "TargetMetadata" : {
+        "TargetSchema" : "",
+        "SupportLobs" : true,
+        "FullLobMode" : false,
+        "LobChunkSize" : 64,
+        "LimitedSizeLobMode" : true,
+        "LobMaxSize" : 32,
+        "InlineLobMaxSize" : 0,
+        "LoadMaxFileSize" : 0,
+        "ParallelLoadThreads" : 0,
+        "ParallelLoadBufferSize" : 0,
+        "BatchApplyEnabled" : false,
+        "TaskRecoveryTableEnabled" : false,
+        "ParallelLoadQueuesPerThread" : 0,
+        "ParallelApplyThreads" : 0,
+        "ParallelApplyBufferSize" : 0,
+        "ParallelApplyQueuesPerThread" : 0
+      },
+      "FullLoadSettings" : {
+        "TargetTablePrepMode" : "DROP_AND_CREATE",
+        "CreatePkAfterFullLoad" : false,
+        "StopTaskCachedChangesApplied" : false,
+        "StopTaskCachedChangesNotApplied" : false,
+        "MaxFullLoadSubTasks" : 8,
+        "TransactionConsistencyTimeout" : 600,
+        "CommitRate" : 10000
+      },
+      "Logging" : {
+        "EnableLogging" : false,
+        "LogComponents" : [
+          {
+            "Id" : "DATA_STRUCTURE",
+            "Severity" : "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            "Id" : "COMMUNICATION",
+            "Severity" : "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            "Id" : "IO",
+            "Severity" : "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            "Id" : "COMMON",
+            "Severity" : "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            "Id" : "FILE_FACTORY",
+            "Severity" : "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            "Id" : "FILE_TRANSFER",
+            "Severity" : "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            "Id" : "REST_SERVER",
+            "Severity" : "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            "Id" : "ADDONS",
+            "Severity" : "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            "Id" : "TARGET_LOAD",
+            "Severity" : "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            "Id" : "TARGET_APPLY",
+            "Severity" : "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            "Id" : "SOURCE_UNLOAD",
+            "Severity" : "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            "Id" : "SOURCE_CAPTURE",
+            "Severity" : "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            "Id" : "TRANSFORMATION",
+            "Severity" : "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            "Id" : "SORTER",
+            "Severity" : "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            "Id" : "TASK_MANAGER",
+            "Severity" : "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            "Id" : "TABLES_MANAGER",
+            "Severity" : "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            "Id" : "METADATA_MANAGER",
+            "Severity" : "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            "Id" : "PERFORMANCE",
+            "Severity" : "LOGGER_SEVERITY_DEFAULT"
+          },
+          {
+            "Id" : "VALIDATOR_EXT",
+            "Severity" : "LOGGER_SEVERITY_DEFAULT"
+          }
+        ],
+
+      },
+      "ControlTablesSettings" : {
+        "historyTimeslotInMinutes" : 5,
+        "ControlSchema" : "",
+        "HistoryTimeslotInMinutes" : 5,
+        "HistoryTableEnabled" : false,
+        "SuspendedTablesTableEnabled" : false,
+        "StatusTableEnabled" : false,
+        "FullLoadExceptionTableEnabled" : false
+      },
+      "StreamBufferSettings" : {
+        "StreamBufferCount" : 3,
+        "StreamBufferSizeInMB" : 8,
+        "CtrlStreamBufferSizeInMB" : 5
+      },
+      "ChangeProcessingDdlHandlingPolicy" : {
+        "HandleSourceTableDropped" : true,
+        "HandleSourceTableTruncated" : true,
+        "HandleSourceTableAltered" : true
+      },
+      "ErrorBehavior" : {
+        "DataErrorPolicy" : "LOG_ERROR",
+        "EventErrorPolicy" : "IGNORE",
+        "DataTruncationErrorPolicy" : "LOG_ERROR",
+        "DataErrorEscalationPolicy" : "SUSPEND_TABLE",
+        "DataErrorEscalationCount" : 0,
+        "TableErrorPolicy" : "SUSPEND_TABLE",
+        "TableErrorEscalationPolicy" : "STOP_TASK",
+        "TableErrorEscalationCount" : 0,
+        "RecoverableErrorCount" : -1,
+        "RecoverableErrorInterval" : 5,
+        "RecoverableErrorThrottling" : true,
+        "RecoverableErrorThrottlingMax" : 1800,
+        "RecoverableErrorStopRetryAfterThrottlingMax" : true,
+        "ApplyErrorDeletePolicy" : "IGNORE_RECORD",
+        "ApplyErrorInsertPolicy" : "LOG_ERROR",
+        "ApplyErrorUpdatePolicy" : "LOG_ERROR",
+        "ApplyErrorEscalationPolicy" : "LOG_ERROR",
+        "ApplyErrorEscalationCount" : 0,
+        "ApplyErrorFailOnTruncationDdl" : false,
+        "FullLoadIgnoreConflicts" : true,
+        "FailOnTransactionConsistencyBreached" : false,
+        "FailOnNoTablesCaptured" : true
+      },
+      "ChangeProcessingTuning" : {
+        "BatchApplyPreserveTransaction" : true,
+        "BatchApplyTimeoutMin" : 1,
+        "BatchApplyTimeoutMax" : 30,
+        "BatchApplyMemoryLimit" : 500,
+        "BatchSplitSize" : 0,
+        "MinTransactionSize" : 1000,
+        "CommitTimeout" : 1,
+        "MemoryLimitTotal" : 1024,
+        "MemoryKeepTime" : 60,
+        "StatementCacheSize" : 50
+      },
+      "PostProcessingRules" : null,
+      "CharacterSetSettings" : null,
+      "LoopbackPreventionSettings" : null,
+      "BeforeImageSettings" : null,
+      "FailTaskWhenCleanTaskResourceFailed" : false,
+      "TTSettings" : null
+    }
+  )
+  lifecycle {
+    ignore_changes = [
+      replication_task_settings,
+    ]
+  }
+  # source_endpoint_arn      = aws_dms_endpoint.omni-wt-rt-updates-source-endpoint.endpoint_arn
+  source_endpoint_arn = var.source_endpoint_arn
+  table_mappings = jsonencode(
+    {
+      "rules" : [
+        {
+          "rule-type" : "selection",
+          "rule-id" : "1",
+          "rule-name" : "1",
+          "object-locator" : {
+            "schema-name" : "dbo",
+            "table-name" : "tbl_RateFile"
+          },
+          "rule-action" : "include",
+          "filters" : []
+        },
+        {
+          "rule-type" : "transformation",
+          "rule-id" : "2",
+          "rule-name" : "2",
+          "rule-target" : "column",
+          "object-locator" : {
+            "schema-name" : "%",
+            "table-name" : "%"
+          },
+          "rule-action" : "add-column",
+          "value" : "transact_id",
+          "expression" : "$AR_H_CHANGE_SEQ",
+          "data-type" : {
+            "type" : "string",
+            "length" : 50
+          }
+        }
+      ]
+  })
+  target_endpoint_arn = aws_dms_endpoint.omni-wt-rt-updates-target-endpoint.endpoint_arn
+  tags = {
+    Application = "Real Time Updates"
+    CreatedBy   = "BizCloudExperts"
+    Environment = var.env
+  }
+}
